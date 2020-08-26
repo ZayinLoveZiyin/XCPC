4c0eae08d80fa4672733fa517cb42eb015d0f3f9
ZayinLoveZiyin <42384458+ZayinLoveZiyin@users.noreply.github.com>
2020-08-26 11:09:22 +0800

    Update and rename polynomial_bf.cpp to polynomial_bf(any prime).cpp

--- templet/polynomial_bf(any prime).cpp
+++ templet/polynomial_bf(any prime).cpp
@@ -34,6 +34,9 @@ namespace polynomial  {
             for (LL &x:*this)
                 cin>>x;
         }
+        operator bool() const {
+            return size();
+        }
     };
     
     ostream& operator << (ostream& os,poly a)   {
@@ -48,6 +51,53 @@ namespace polynomial  {
         a.resize(n);
         for (LL &x:a) is>>x;
         return is;
+    }
+
+    namespace FFT   {
+        #define maxn 1000050
+        const double PI=acos(-1);
+
+        int N,rev[maxn];
+        struct comp {
+            double x,y;
+            comp(double _x=0,double _y=0) : x(_x),y(_y) {
+            }
+            comp operator + (const comp& C) {   return comp(x+C.x,y+C.y);   }
+            comp operator - (const comp& C) {   return comp(x-C.x,y-C.y);   }
+            comp operator * (const comp& C) {   return comp(x*C.x-y*C.y,x*C.y+y*C.x);   }
+            comp operator / (const double& l) {   return comp(x/l,y/l);   }
+        } w[maxn];
+        void init(int n) {
+            for (N=1;N<n;N<<=1);
+            assert(N<maxn);
+
+            rev[0]=0,rev[1]=N>>1;
+            for (int i=2;i<N;++i)
+                rev[i]=rev[i>>1]>>1|rev[i&1];
+            
+            for (int i=0;i<N;++i)
+                w[i]=comp(cos(2*PI*i/N),sin(2*PI*i/N));
+        }
+        void DFT(comp *A)   {
+            for (int i=0;i<N;++i)
+                if (i<rev[i])
+                    swap(A[i],A[rev[i]]);
+            for (int len=2;len<=N;len<<=1)  {
+                int m=len>>1;
+                for (comp *p=A;p!=A+N;p+=len)
+                    for (int i=0;i<m;++i)   {
+                        comp u=p[i],v=p[i+m]*w[N/len*i];
+                        p[i]=u+v;
+                        p[i+m]=u-v;
+                    }
+            }
+        }
+        void IDFT(comp *A)    {
+            DFT(A);
+            reverse(A+1,A+N);
+            for (int i=0;i<N;++i)
+                A[i]=A[i]/N;
+        }
     }
 
     inline poly operator + (const poly& a,const poly& b)   {
@@ -79,13 +129,47 @@ namespace polynomial  {
         return a;
     } 
 
+    const int lim=100;
     poly operator * (const poly &a,const poly &b) {
         poly ans(a.size()+b.size());
-        for (int i=0;i<a.size();++i)
-            for (int j=0;j<b.size();++j)
-                add(ans[i+j],a[i]*b[j]);
+        if (a.size()<lim&&b.size()<lim) {
+            for (int i=0;i<a.size();++i)
+                for (int j=0;j<b.size();++j)
+                    add(ans[i+j],a[i]*b[j]);
+            return ans.normalize();
+        } else  {
+            static const int w=1000;
+            static FFT::comp ta[3][maxn],tb[3][maxn],tc[maxn];
+
+            FFT::init(a.size()+b.size());
+            auto init=[&](const poly& a,FFT::comp ta[3][maxn])   {
+                for (int i=0;i<FFT::N;++i)
+                    for (int c=0;c<3;++c)
+                        ta[c][i]=0;
+                for (int i=0;i<a.size();++i)    {
+                    LL x=a[i];
+                    for (int c=0;c<3;++c)
+                        ta[c][i]=x%w,x/=w;
+                }
+                for (int c=0;c<3;++c)
+                    FFT::DFT(ta[c]);
+            };
+            init(a,ta),init(b,tb);
+
+            for (int ca=0;ca<3;++ca)
+                for (int cb=0;cb<3;++cb)    {
+                    LL cw=pw(w,ca+cb);
+                    for (int i=0;i<FFT::N;++i) tc[i]=ta[ca][i]*tb[cb][i];
+                    FFT::IDFT(tc);
+                    for (int i=0;i<ans.size();++i)
+                        add(ans[i],(LL)(tc[i].x+0.5)*cw);
+                }
+
+            return ans.normalize();
+        }
         return ans.normalize();
     }
+
     poly operator / (poly a,const poly &b) {
         if (a.size()<b.size()) return poly{};
 
@@ -101,6 +185,7 @@ namespace polynomial  {
         reverse(ans.begin(),ans.end());
         return ans.normalize();
     }
+
     poly operator % (const poly &a,const poly &b) {
         return (a-(a/b)*b).normalize();
     }
